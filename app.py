2c05d94cfcd9df28685811a064a26dddddb1214c
Yashking88 <95337841+Yashking88@users.noreply.github.com>
2024-09-04 18:40:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+import os
+from flask import  Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def load_form():
+    return render_template('upload.html')
+
+
+@app.route('/gray', methods=['POST'])
+def upload_image():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+
+    file_data = make_grayscale(file.read())
+    with open(os.path.join('static/', filename), 
+        'wb') as f:
+        f.write(file_data)
+
+        # ends here
+
+    display_message = 'Image successfully uploaded and displayed below'
+    return render_template('upload.html', filename=filename, message = display_message)
+
+
+# Write the make_grayscale() function below
+
+def make_grayscale(input_image):
+    image_arry = np.fromstring(input_image, dtype='uint8')
+    print("Image Array:", image_arry)
+
+    decode_array_to_img = cv2.imdecode(image_arry, cv2.IMREAD_UNCHANGED)
+    print("Decode value of image: ", decode_array_to_img)
+    
+    converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_BGR2GRAY)
+    status, output_image = cv2.imencode('.PNG', converted_gray_img)
+    print('status:', status)
+
+    return output_image
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# make_grayscale() function ends above
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename=filename))
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
